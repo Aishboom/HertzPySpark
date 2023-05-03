b8a042ea7f004be35bdd5ed2d0c6aa3a9a68be20
Rittik De <rittik.de@tami.co.uk>
2023-05-03 20:43:30 +0100

    code updated along with all the comments. README.md and requirements.txt added

--- Hertz_Data_Procesing.py
+++ Hertz_Data_Procesing.py
@@ -9,244 +9,207 @@ from pyspark.sql.types import (
     StructField, DateType,
 )
 
-# create a SparkSession
-spark = SparkSession.builder.appName("Big Data Processing JSON Files")\
-    .config("spark.sql.warehouse.dir", "file:/C:/tempwarhouse/") \
-    .config("spark.sql.shuffle.partitions", 100)\
-    .getOrCreate()
+def car_diagonastic():
+    # create a SparkSession
+    spark = SparkSession.builder.appName("Big Data Processing JSON Files")\
+        .config("spark.sql.warehouse.dir", "file:/C:/tempwarhouse/") \
+        .config("spark.sql.shuffle.partitions", 100)\
+        .getOrCreate()
 
-# specify the path to the folder containing JSON files
-folder_path = "events/"
-output_path = "file:/C:/tempwarhouse/"
+    # specify the path to the folder containing JSON files
+    folder_path = "events/"
+    output_path = "file:/C:/tempwarhouse/"
 
-# define schema
-new_schema = StructType(
-    [
-        StructField("timestamp", StringType(), True),
-        StructField("type", StringType(), True),
-        StructField("subtype", StringType(), True),
-        StructField("eventId", StringType(), True),
-        StructField(
-            "eventData",
-            StructType(
-                [
-                    StructField("latitude", StringType(), True),
-                    StructField("longitude", StringType(), True),
-                    StructField("sensor", StringType(), True),
-                    StructField(
-                        "sensorData",
-                        StructType(
-                            [
-                                StructField("mileage", DoubleType(), True),
-                                StructField("fuelLevel", DoubleType(), True),
-                                StructField("fuelAmount", StringType(), True),
-                                StructField("batteryLevel", StringType(), True),
-                                StructField("remainingLife", StringType(), True),
-                                StructField(
-                                    "wheels",
-                                    ArrayType(
-                                        StructType(
-                                            [
-                                                StructField("axle", IntegerType(), True),
-                                                StructField("side", StringType(), True),
-                                                StructField("position", StringType(), True),
-                                                StructField("pressure", DoubleType(), True),
-                                                StructField("uom", StringType(), True),
-                                            ]
-                                        )
+    # define schema for the JSON files
+    new_schema = StructType(
+        [
+            StructField("timestamp", StringType(), True),
+            StructField("type", StringType(), True),
+            StructField("subtype", StringType(), True),
+            StructField("eventId", StringType(), True),
+            StructField(
+                "eventData",
+                StructType(
+                    [
+                        StructField("latitude", StringType(), True),
+                        StructField("longitude", StringType(), True),
+                        StructField("sensor", StringType(), True),
+                        StructField(
+                            "sensorData",
+                            StructType(
+                                [
+                                    StructField("mileage", DoubleType(), True),
+                                    StructField("fuelLevel", DoubleType(), True),
+                                    StructField("fuelAmount", StringType(), True),
+                                    StructField("batteryLevel", StringType(), True),
+                                    StructField("remainingLife", StringType(), True),
+                                    StructField(
+                                        "wheels",
+                                        ArrayType(
+                                            StructType(
+                                                [
+                                                    StructField("axle", IntegerType(), True),
+                                                    StructField("side", StringType(), True),
+                                                    StructField("position", StringType(), True),
+                                                    StructField("pressure", DoubleType(), True),
+                                                    StructField("uom", StringType(), True),
+                                                ]
+                                            )
+                                        ),
                                     ),
-                                ),
-                            ]
-                        ), True, ), ]), True, ),
-        StructField("vehicleId", StringType(), True),
-    ]
-)
-# read all JSON files from the folder
-# df = spark.read.format("json").schema(new_schema).load(folder_path)
+                                ]
+                            ), True, ), ]), True, ),
+            StructField("vehicleId", StringType(), True),
+        ]
+    )
 
-df = spark.read.format("json").option("multiLine", "true").option("inferSchema","true").load(folder_path)
+    # read all JSON files from the folder with the defined schema
+    df = spark.read.format("json").schema(new_schema).load(folder_path)
 
-df = df.repartition("type")
+    # perform repartition based on the number of executor.instance * executor.cores
+    df = df.repartition("type")
 
-#
-# Add filename column to dataframe
-df = df.withColumn("filename", input_file_name())
+    # add a filename column to dataframe to check which filename has the schema mismatch
+    #df = df.withColumn("filename", input_file_name())
 
-# df.select('eventData.sensor').show(truncate=False)
+    # select relevant columns and rename some for readability
+    df = df.select(
+        col("timestamp"),
+        col("type"),
+        col("subtype"),
+        col("eventId"),
+        col("eventData.latitude").alias("latitude"),
+        col("eventData.longitude").alias("longitude"),
+        col("eventData.sensor").alias("sensor"),
+        col("eventData.sensorData.mileage").alias("mileage"),
+        col("eventData.sensorData.fuelLevel").alias("fuelLevel"),
+        col("eventData.sensorData.fuelAmount").alias("fuelAmount"),
+        col("eventData.sensorData.batteryLevel").alias("batteryLevel"),
+        col("eventData.sensorData.remainingLife").alias("remainingLife"),
+        col("eventData.sensorData.wheels").alias("wheels"),
+        col("vehicleId"),
+        to_date(col("timestamp")).alias("date"),
+        hour("timestamp").alias("Hour"),
+        col("filename")
+    )
 
-df = df.select(
-    col("timestamp"),
-    col("type"),
-    col("subtype"),
-    col("eventId"),
-    col("eventData.latitude").alias("latitude"),
-    col("eventData.longitude").alias("longitude"),
-    col("eventData.sensor").alias("sensor"),
-    col("eventData.sensorData.mileage").alias("mileage"),
-    col("eventData.sensorData.fuelLevel").alias("fuelLevel"),
-    col("eventData.sensorData.fuelAmount").alias("fuelAmount"),
-    col("eventData.sensorData.batteryLevel").alias("batteryLevel"),
-    col("eventData.sensorData.remainingLife").alias("remainingLife"),
-    col("eventData.sensorData.wheels").alias("wheels"),
-    col("vehicleId"),
-    to_date(col("timestamp")).alias("date"),
-    hour("timestamp").alias("Hour"),
-    col("filename")
-)
+    # print schema and number of records in dataframe
+    #df.show(truncate=False)
 
-# print schema
-# print(df.printSchema())
+    # separate location events and diagnostic events into two dataframes
 
-# print number of records in dataframes
-df.show(truncate=False)
-
-location_events_df = df.select(
-    col("timestamp"),
-    col("subtype"),
-    col("eventId"),
-    col("latitude"),
-    col("longitude"),
-    col("vehicleId"),
-    col("date"),
-    col("Hour")
-).filter(col("type") == "LOCATION")
-diagnostic_events_df = (
-    df.select(
+    location_events_df = df.select(
         col("timestamp"),
         col("subtype"),
         col("eventId"),
-        col("sensor"),
-        col("mileage"),
-        col("fuelLevel"),
-        col("fuelAmount"),
-        col("batteryLevel"),
-        col("remainingLife"),
-        explode(col("wheels")).alias("wheels"),
+        col("latitude"),
+        col("longitude"),
         col("vehicleId"),
         col("date"),
         col("Hour")
-    ).select(
-        "timestamp",
-        "subtype",
-        "eventId",
-        "sensor",
-        "mileage",
-        "fuelLevel",
-        "fuelAmount",
-        "batteryLevel",
-        "remainingLife",
-        "wheels.axle",
-        "wheels.side",
-        "wheels.position",
-        "wheels.pressure",
-        "wheels.uom",
-        "vehicleId",
-        "date",
-        "Hour"
-    ).filter(col("type") == "DIAGNOSTIC")
-)
+    ).filter(col("type") == "LOCATION")
+    diagnostic_events_df = (
+        df.select(
+            col("timestamp"),
+            col("subtype"),
+            col("eventId"),
+            col("sensor"),
+            col("mileage"),
+            col("fuelLevel"),
+            col("fuelAmount"),
+            col("batteryLevel"),
+            col("remainingLife"),
+            explode(col("wheels")).alias("wheels"),
+            col("vehicleId"),
+            col("date"),
+            col("Hour")
+        ).select(
+            "timestamp",
+            "subtype",
+            "eventId",
+            "sensor",
+            "mileage",
+            "fuelLevel",
+            "fuelAmount",
+            "batteryLevel",
+            "remainingLife",
+            "wheels.axle",
+            "wheels.side",
+            "wheels.position",
+            "wheels.pressure",
+            "wheels.uom",
+            "vehicleId",
+            "date",
+            "Hour"
+        ).filter(col("type") == "DIAGNOSTIC")
+    )
 
-location_events_schema = StructType(
-    [
-            StructField("timestamp", StringType(), True),
-            StructField("subtype", StringType(), True),
-            StructField("eventId", StringType(), True),
-            StructField("latitude", StringType(), True),
-            StructField("longitude", StringType(), True),
-            StructField("vehicleId", StringType(), True),
-            StructField("date", DateType(), True),
-            StructField("Hour", IntegerType(), True)
-    ]
-)
+    location_events_schema = StructType(
+        [
+                StructField("timestamp", StringType(), True),
+                StructField("subtype", StringType(), True),
+                StructField("eventId", StringType(), True),
+                StructField("latitude", StringType(), True),
+                StructField("longitude", StringType(), True),
+                StructField("vehicleId", StringType(), True),
+                StructField("date", DateType(), True),
+                StructField("Hour", IntegerType(), True)
+        ]
+    )
 
-# fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
-# fs.mkdirs(spark._jvm.org.apache.hadoop.fs.Path(output_path + "location_events_output"))
+    # fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
+    # fs.mkdirs(spark._jvm.org.apache.hadoop.fs.Path(output_path + "location_events_output"))
 
-numBuckets = 2
+    numBuckets = 2
 
-location_events_df \
-    .repartition(numBuckets, "eventId") \
-    .write \
-    .format("parquet") \
-    .partitionBy("date","Hour") \
-    .mode("overwrite") \
-    .bucketBy(numBuckets, "eventId") \
-    .option("compression", "snappy") \
-    .option("mergeSchema", "true") \
-    .option("schema", location_events_schema)\
-    .option("path", output_path + "location_events_output")\
-    .saveAsTable("location_events")
+    location_events_df \
+        .repartition(numBuckets, "eventId") \
+        .write \
+        .format("parquet") \
+        .partitionBy("date","Hour") \
+        .mode("overwrite") \
+        .bucketBy(numBuckets, "eventId") \
+        .option("compression", "snappy") \
+        .option("mergeSchema", "true") \
+        .option("schema", location_events_schema)\
+        .option("path", output_path + "location_events_output")\
+        .saveAsTable("location_events")
 
-# location_events_df\
-#   .write\
-#   .format("parquet")\
-#   .partitionBy("date")\
-#   .mode("overwrite")\
-#   .bucketBy(numBuckets, "eventId")\
-#   .option("compression", "snappy")\
-#   .option("mergeSchema", "true")\
-#   .option("path", output_path + "location_events_output")\
-#   .saveAsTable("location_events")
 
-# location_events_df\
-#   .write\
-#   .format("parquet")\
-#   .partitionBy("date")\
-#   .mode("overwrite")\
-#   .option("compression", "snappy")\
-#   .option("mergeSchema", "true")\
-#   .save(output_path + "location_events_output")
+    diagnostic_events_schema = StructType(
+        [
+                StructField("timestamp", StringType(), True),
+                StructField("subtype", StringType(), True),
+                StructField("eventId", StringType(), True),
+                StructField("sensor", StringType(), True),
+                StructField("mileage", DoubleType(), True),
+                StructField("fuelLevel", DoubleType(), True),
+                StructField("fuelAmount", StringType(), True),
+                StructField("batteryLevel", StringType(), True),
+                StructField("remainingLife", StringType(), True),
+                StructField("axle", IntegerType(), True),
+                StructField("side", StringType(), True),
+                StructField("position", StringType(), True),
+                StructField("pressure", DoubleType(), True),
+                StructField("uom", StringType(), True),
+                StructField("vehicleId", StringType(), True),
+                StructField("date", DateType(), True),
+                StructField("Hour", IntegerType(), True)
+        ]
+    )
+    # fs.mkdirs(spark._jvm.org.apache.hadoop.fs.Path(output_path + "diagnostic_events_output"))
+    diagnostic_events_df \
+        .repartition(numBuckets, "eventId") \
+        .write \
+        .format("parquet") \
+        .partitionBy("date", "Hour") \
+        .mode("overwrite") \
+        .bucketBy(numBuckets, "eventId") \
+        .option("compression", "snappy") \
+        .option("mergeSchema", "true") \
+        .option("schema", diagnostic_events_schema)\
+        .option("path", output_path + "diagnostic_events_output")\
+        .saveAsTable("diagnostic_events")
 
-diagnostic_events_schema = StructType(
-    [
-            StructField("timestamp", StringType(), True),
-            StructField("subtype", StringType(), True),
-            StructField("eventId", StringType(), True),
-            StructField("sensor", StringType(), True),
-            StructField("mileage", DoubleType(), True),
-            StructField("fuelLevel", DoubleType(), True),
-            StructField("fuelAmount", StringType(), True),
-            StructField("batteryLevel", StringType(), True),
-            StructField("remainingLife", StringType(), True),
-            StructField("axle", IntegerType(), True),
-            StructField("side", StringType(), True),
-            StructField("position", StringType(), True),
-            StructField("pressure", DoubleType(), True),
-            StructField("uom", StringType(), True),
-            StructField("vehicleId", StringType(), True),
-            StructField("date", DateType(), True),
-            StructField("Hour", IntegerType(), True)
-    ]
-)
-# fs.mkdirs(spark._jvm.org.apache.hadoop.fs.Path(output_path + "diagnostic_events_output"))
-diagnostic_events_df \
-    .repartition(numBuckets, "eventId") \
-    .write \
-    .format("parquet") \
-    .partitionBy("date", "Hour") \
-    .mode("overwrite") \
-    .bucketBy(numBuckets, "eventId") \
-    .option("compression", "snappy") \
-    .option("mergeSchema", "true") \
-    .option("schema", diagnostic_events_schema)\
-    .option("path", output_path + "diagnostic_events_output")\
-    .saveAsTable("diagnostic_events")
-# diagnostic_events_df\
-#   .write\
-#   .format("parquet")\
-#   .partitionBy("date")\
-#   .mode("overwrite")\
-#   .bucketBy(numBuckets, "eventId")\
-#   .option("compression", "snappy")\
-#   .option("mergeSchema", "true")\
-#   .option("path", output_path + "diagnostic_events_output")\
-#   .saveAsTable("diagnostic_events")
-
-# diagnostic_events_df\
-#   .write\
-#   .format("parquet")\
-#   .partitionBy("date")\
-#   .mode("overwrite")\
-#   .option("compression", "snappy")\
-#   .option("mergeSchema", "true")\
-#   .save(output_path + "diagnostic_events_output")
+if __name__=="__main__":
+    car_diagonastic()
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # HertzPySpark
+## Setup and verify the solution
+
+The following instructions will guide you to set up and verify the solution:
+
+### Prerequisites
+
+* Python 3.6 or later
+* Apache Spark 2.4.0 or later
+
+### Installation
+
+1. Clone the repository:
+```
+git clone https://github.com/Aishboom/HertzPySpark.git
+```
+2. Create and activate a virtual environment:
+```
+python3 -m venv venv
+source venv/bin/activate
+```
+3. Install the required packages:
+```
+pip install -r requirements.txt
+```
+
+### Usage
+
+1. Specify the path to the folder containing JSON files and the output path in the `Hertz_Data_Procesing.py` file:
+```python
+folder_path = "events/"
+output_path = "file:/path/output"
+```
+
+2. Run the script:
+```
+python Hertz_Data_Procesing.py
+```
+
+### Verification
+
+1. Check that the output files are generated in the output path specified in the `Hertz_Data_Procesing.py` file.
+
+2. Verify that the output files have the expected schema and data.
+
+3. To ensure the partition hierarchy, the table_name should be the first level, followed by the date, and then by hour. 
+   Additionally, the number of buckets mentioned is 2. The choice of the number of buckets is generally based on the number 
+   of `executor_instances*executor_cores`. This partitioning scheme allows for efficient querying of the data based on the date and time, 
+   while the number of buckets ensures that the data is evenly distributed across the cluster for parallel processing.
+
+4. If you encounter any errors, please check the logs in the console or the log files in the logs directory.
